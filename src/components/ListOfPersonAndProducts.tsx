abedadaec873adb7cbbcc6b328ab7aa938dc61b4
Jorge <jorge.perez@meltstudio.co>
2022-10-14 21:16:54 -0500

    feat: add products to lists

--- src/components/ListOfPersonAndProducts.tsx
+++ src/components/ListOfPersonAndProducts.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import RegularList from "./Layouts/Lists/RegularList";
 import PersonList from "./Layouts/People";
 import people from "./data/People.json";
+import products from "./data/Products.json";
+import ProductList from "./Layouts/Products";
 const ListOfPersonAndProducts = () => {
   return (
     <>
@@ -16,6 +18,20 @@ const ListOfPersonAndProducts = () => {
         items={people}
         resourceName="person"
         itemComponent={PersonList.Large}
+      />
+      <h2>Product small data</h2>
+      <RegularList
+        items={products}
+        resourceName="product"
+        itemComponent={ProductList.Small}
+        numbered
+      />
+      <h2>Product full data</h2>
+      <RegularList
+        items={products}
+        resourceName="product"
+        itemComponent={ProductList.Large}
+        numbered
       />
     </>
   );

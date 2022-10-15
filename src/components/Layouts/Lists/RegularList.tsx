a5bae6aa363d505963f72fa81b5ca8ca5600f4e0
Jorge <jorge.perez@meltstudio.co>
2022-10-14 21:15:35 -0500

    feat: add numbered property and title to reglist

--- src/components/Layouts/Lists/RegularList.tsx
+++ src/components/Layouts/Lists/RegularList.tsx
@@ -4,16 +4,19 @@ interface ListProps {
   items: any[];
   resourceName: string;
   itemComponent: (item: any) => JSX.Element;
+  numbered?: boolean;
 }
 const RegularList = ({
   items,
   resourceName,
   itemComponent: ItemComponent,
+  numbered = false,
 }: ListProps) => {
   return (
     <>
       {items.map((item, i) => (
         <>
+          {numbered && <h4>{i + 1}</h4>}
           <ItemComponent
             {...{ [resourceName]: item }}
             key={`resourceName-${i}`}

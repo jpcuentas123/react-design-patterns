cb84dc61a2cbe3fb3f99b29ed42f9926a6454b06
Jorge <jorge.perez@meltstudio.co>
2022-10-26 07:42:47 -0500

    chore: split modal styles into new file

---
+++ src/components/Layouts/Modal/Styles.tsx
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+
+const ModalContainer = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ModalContent = styled.div`
+  background-color: white;
+  border-radius: 10px;
+  padding: 20px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+  width: 500px;
+  min-height: 300px;
+`;
+
+const ModalHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+  border-bottom: 1px solid #eee;
+`;
+
+const SModal = {
+  ModalContainer,
+  ModalContent,
+  ModalHeader,
+};
+
+export default SModal;
